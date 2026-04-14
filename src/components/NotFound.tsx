7f69c0b5912852f29dd95e0007897edfb45dc0a5
Abubakar dahir hassan <Abu112abu112abu112@gmail.com>
2026-04-14 13:00:52 +0530

    styling the not found page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,28 +2,28 @@ import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen bg-[#F9F509] flex items-center justify-center px-4">
-      <div className="bg-black text-white rounded-2xl shadow-2xl p-10 max-w-md w-full text-center">
+    <div className="min-h-screen flex items-center justify-center px-4">
+      <div className="border-2 border-white text-white rounded-2xl shadow-2xl py-16 xl:px-20 max-w-md w-full text-center">
         
         {/* 404 */}
-        <h1 className="text-7xl font-extrabold tracking-tight animate-bounce">
+        <h1 className="text-7xl max-md:text-5xl font-extrabold tracking-tight animate-bounce text-p3">
           404
         </h1>
 
         {/* Title */}
-        <h2 className="text-2xl font-semibold mt-4">
+        <h2 className="text-2xl max-md:text-xl font-semibold mt-4">
           Oops! Page not found
         </h2>
 
         {/* Description */}
-        <p className="text-gray-400 mt-2 mb-6">
+        <p className="text-gray-400 mt-2 mb-6 max-md:text-sm">
           The page you're looking for doesn’t exist or has been moved.
         </p>
 
         {/* Button */}
         <Link
           to="/"
-          className="inline-block bg-[#F9F509] text-black font-semibold px-6 py-2 rounded-lg hover:scale-105 transition-transform duration-200"
+          className="inline-block py-4 bg-p3 text-black font-semibold px-6 rounded-lg hover:scale-105 transition-transform duration-200 max-md:text-sm"
         >
           Go Home
         </Link>
